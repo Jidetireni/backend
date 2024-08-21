6ace6020208cd988391573c462a8f29b7e8c1dde
jidetireni <jtirenipraise@gmail.com>
2024-08-21 17:05:02 +0100

    TCP server code

---
+++ TCP/server/tcpserver.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func echoUpper(w io.Writer, r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Fprintf(w, "%s\n", strings.ToUpper(line))
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("error: %s", err)
+	}
+}
+func main() {
+	const name = "tcpupperecho"
+	log.SetPrefix(name + "\t")
+
+	port := flag.Int("p", 8080, "port to listen on")
+	flag.Parse()
+
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{Port: *port})
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	log.Printf("listening at localhost: %s", listener.Addr())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Accepting connection from %s", conn.RemoteAddr())
+		go echoUpper(conn, conn)
+	}
+}
